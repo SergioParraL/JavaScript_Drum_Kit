6ce106ecac3e7699447e4a1dbabfdd448af79ee3
Sergio Parra <sergiolparra666@gmail.com>
2021-08-14 22:27:02 -0430

    agregado: fondo. se cambio los bordes y escala a precionar las letras

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-window.addEventListener('keydown',function (e) {
+function playSound (e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 	if (!audio) return;
 	audio.currenTime = 0
 	audio.play()	
-})
+	key.classList.add('key-Press')
+}
+
+function remove_Key_Press(e){
+	if(e.propertyName !== 'transform') return
+	this.classList.remove('key-Press')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend',remove_Key_Press))
+window.addEventListener('keydown',playSound)
+
